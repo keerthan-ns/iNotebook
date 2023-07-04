8b186f3f738717f09eae857a6f237542e6ac3229
Keerthan N S <keerthanns091@gmail.com>
2023-07-04 18:39:33 +0530

    [add]basic login req

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../models/User')
 
-router.get('/',(req,res)=>{
-    obj = {
-        a: "thor",
-        number: 34
-    }
-    res.json(obj)
+// create a User using: POST "/api/auth". Auth not required
+router.post('/',(req,res)=>{
+    const user = User(req.body)
+    user.save()
+    res.json(req.body)
+    
 })
 
 module.exports = router
