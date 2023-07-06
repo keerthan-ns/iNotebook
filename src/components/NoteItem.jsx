fa4215697c3dbd44b5d6bfd98d22607872797186
Keerthan N S <keerthanns091@gmail.com>
2023-07-06 23:47:39 +0530

    [add]badge-tag

--- src/components/NoteItem.jsx
+++ src/components/NoteItem.jsx
@@ -9,9 +9,10 @@ export default function NoteItem(props) {
     return (
         <>
             <div className="block max-w-sm pt-6 pb-4 px-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{note.title}</h5>
-                <p className="font-normal text-gray-700 dark:text-gray-400">{note.description}</p>
-                <div className='flex flex-row justify-left items-center gap-4'>
+                <span className="bg-pink-600 text-white text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full">{note.tag?note.tag:"Default"}</span>
+                <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">{note.title}</h5>
+                <p className="font-normal text-gray-700 dark:text-gray-400 line-clamp-2">{note.description}</p>
+                <div className='mt-2 flex flex-row justify-left items-center gap-4'>
                     <i className="cursor-pointer fa-solid fa-pen text-[#45e766] text-lg" onClick={()=>{updateNote(note)}}></i>
                     <i className="cursor-pointer fa-solid fa-trash text-[#eb1010] text-lg" onClick={()=>{deleteNote(note._id)}}></i>
                 </div>
@@ -25,6 +26,7 @@ NoteItem.propTypes = {
     note: PropTypes.shape({
         _id: PropTypes.string.isRequired,
         title: PropTypes.string.isRequired,
+        tag: PropTypes.string.isRequired,
         description: PropTypes.string.isRequired,
     }),
     updateNote: PropTypes.func,
